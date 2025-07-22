2a5662a5c8eb2bbd11551e320b7ae3d70315b033
Noor Diab <nd00034@mix.wvu.edu>
2025-07-22 19:26:11 -0400

    Added a backend for the login/sign up pages

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,6 +3,12 @@
 
 #imports
 import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app= Flask(__name__)
+
+id=0
 
 ###DATABASE SECTION###:
     #to create: 
@@ -10,20 +16,38 @@ import sqlite3
             #quotes saved db (create later??)
 
     #create user data base    
+
 connect= sqlite3.connect('user.db')
 connect.execute("DROP TABLE IF EXISTS USER")
 
-#create a table
+    #create a table
 connect.execute('''CREATE TABLE USER
                 (ID INT PRIMARY KEY NOT NULL
-                EMAIL TEXT NOT NULL
                 USERNAME TEXT NOT NULL
                 PASSWORD TEXT NOT NULL);
-''')
+    ''')
+
+
+
+
+
+@app.route("/user/db", methods=["POST"])
+
+def receive_data():
+    if request.is_json: 
+        data=request.json
+        username= data.get('username')
+        password= data.get('password')
+        id= id+1
+
+    connect.execute("INSERT INTO USER (ID, USERNAME, PASSWORD) VALUES (id, username, password)")
+
+    
+connect.close()
 
 #figure out how to put the values in the table...
 
-connect.close()
+
 
 ###API SECTION ###
 
