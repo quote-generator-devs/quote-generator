8695ed803d1c2345d7646dbd17692d9c812c6ddb
Noor Diab <nd00034@mix.wvu.edu>
2025-07-23 13:18:11 -0400

    Added a handleSubmit() to the Login function in App.js

--- src/App.js
+++ src/App.js
@@ -84,6 +84,25 @@ function Home() {
 }
 
 function Login() {
+
+    function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    // addUser(formData); // plaintext(?) format (seems less useful)
+    // console.log(formData);
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson); // JSON formatted (seems more useful)
+    validateUser(formJson);
+  }
+
   return (
     <div class = "login_page"> 
       <div class = "login_header">
