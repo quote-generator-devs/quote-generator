d07215cbcf63c07cda140a30a1cc592867c1df0e
bhavana-pixel <bhavana0412@hotmail.com>
2025-08-06 13:46:32 -0400

    Added linkedin and github links to the about us page.

--- src/App.js
+++ src/App.js
@@ -249,23 +249,48 @@ function AboutUs() {
           <div class="dev">
             <img src="/images/Bhavana.jpg" alt="Image of Bhavana Dakshinamoorthy" class="bhavana"/>
             <p>Bhavana Dakshinamoorthy</p>
+            <a href = "https://www.linkedin.com/in/bhavana-dakshinamoorthy-874a5a307">
+              <img src = "/images/Linkedin-Logo.png" alt= "Image of Linkedin Logo" class = "linkedin"/>
+            </a>
+            <a href = "https://github.com/bhavana-pixel">
+              <img src = "/images/Github-Logo.png" alt="Image of Github Logo" class = "github"/>
+            </a>
           </div>
-          <div class="dev">
+          <div class="dev"> 
             <img src="/images/Noor.jpg" alt="Image of Noor Diab" class="noor" />
             <p>Noor Diab</p>
-            <p class = "linkedin">Linkedin: </p>
+            <a href = "https://www.linkedin.com/in/noordiab">
+              <img src = "/images/Linkedin-Logo.png" alt="Image of Linkedin Logo" class = "linkedin"/>
+            </a>
+            <a href = "https://github.com/noordiab05">
+              <img src = "/images/Github-Logo.png" alt="Image of Github Logo" class = "github"/>
+            </a>
           </div>
           <div class="dev">
             <img src="/images/Danny.jpg" alt="Image of Daniel Lawler"  class="danny"/>
             <p>Daniel Lawler</p>
+            <a href = "https://www.linkedin.com/in/daniel-lawler-a8381b337/">
+              <img src = "/images/Linkedin-Logo.png" alt= "Image of Linkedin Logo" class = "linkedin"/>
+            </a>
+            <a href = "https://github.com/dannylawler">
+              <img src = "/images/Github-Logo.png" alt="Image of Github Logo" class = "github"/>
+            </a>
           </div>
           <div class="dev">
             <img src="https://placehold.co/200" alt="Image of Kaden Spencer" class="kaden"/>
             <p>Kaden Spencer</p>
+              <img src = "/images/Linkedin-Logo.png" alt= "Image of Linkedin Logo" class = "linkedin"/>         
+              <img src = "/images/Github-Logo.png" alt="Image of Github Logo" class = "github"/>
           </div>
           <div class="dev">
             <img src="/images/Shane.jpg" alt="Image of Shane Thoma"  class="shane"/>
             <p>Shane Thoma</p>
+            <a href = "https://www.linkedin.com/in/shanethoma/">
+              <img src = "/images/Linkedin-Logo.png" alt= "Image of Linkedin Logo" class = "linkedin"/>
+            </a>
+            <a href = "https://github.com/shane-thoma">
+              <img src = "/images/Github-Logo.png" alt="Image of Github Logo" class = "github"/>
+            </a>
           </div>
         </div>
       </div>
